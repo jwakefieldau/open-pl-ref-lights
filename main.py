5604f27fbc64e35bedd15cb32e1bed6e016c1026
James <jmw1982@gmail.com>
2019-08-01 13:52:34 +1000

    toggle dummy lift timer and light display on keypresses, exit on specific key

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from gi.repository import Gtk, Gdk, GdkPixbuf, GObject
 
 import datetime
 
-def next_att_timer_tick(label_widget, span_size,  start_dt):
+def timer_tick(label_widget, span_size,  start_dt, max_timer_seconds=60, prefix_str=''):
 
     # return true to fire again
     ret = False
 
     cur_dt = datetime.datetime.now()
     timer_td = cur_dt - start_dt
-    timer_total_countdown_seconds = int(60 - timer_td.seconds)
+    timer_total_countdown_seconds = int(max_timer_seconds - timer_td.seconds)
 
     # don't let it go negative
     if timer_total_countdown_seconds > 0:
@@ -31,16 +31,49 @@ def next_att_timer_tick(label_widget, span_size,  start_dt):
     else:
         timer_sec_str = str(timer_sec)   
 
-    label_widget.set_markup('<span size="{}" foreground="white">Next attempt submission: {}:{}</span>'.format(span_size, timer_min, timer_sec_str))
+    label_widget.set_markup('<span size="{}" foreground="white">{} {}:{}</span>'.format(span_size, prefix_str, timer_min, timer_sec_str))
 
     return ret
 
 
-window = Gtk.Window(title='Ref lights')
-window.fullscreen()
+def handle_key_press(window, event):
+
+    #TODO - actually do input control, for now just toggle lift timer and lights
+    if event.keyval == ord('f'):
+        lift_timer_window.hide()
+        light_window.show_all()
+
+    if event.keyval == ord('j'):
+        light_window.hide()
+        lift_timer_window.show_all()
+
+    if event.keyval == ord('x'):
+        Gtk.main_quit()
+
+
+#TODO - make this actually do something - mockup
+lift_timer_window = Gtk.Window(title='Lift Timer')
+lift_timer_window.fullscreen()
+
+lift_timer_bg_eventbox = Gtk.EventBox()
+lift_timer_window.add(lift_timer_bg_eventbox)
+
+black_color = Gdk.Color(red=0, green=0, blue=0)
+lift_timer_bg_eventbox.modify_bg(Gtk.StateType.NORMAL, black_color)
+
+lift_timer_label = Gtk.Label()
+lift_timer_bg_eventbox.add(lift_timer_label)
+
+#TODO - ue scaling on span size
+lift_timer_label.set_markup('<span size="240000" foreground="white">01:00</span>')
+
+lift_timer_window.show_all()
+
+light_window = Gtk.Window(title='Ref lights')
+light_window.fullscreen()
 
 bg_eventbox = Gtk.EventBox()
-window.add(bg_eventbox)
+light_window.add(bg_eventbox)
 vbox = Gtk.VBox(False, 4)
 bg_eventbox.add(vbox)
 black_color = Gdk.Color(red=0, green=0, blue=0)
@@ -49,8 +82,7 @@ bg_eventbox.modify_bg(Gtk.StateType.NORMAL, black_color)
 
 #TODO - setup input devices
 
-#TODO - get window dimensions and apply scaling to lights and font
-screen = window.get_screen()
+screen = light_window.get_screen()
 screen_width = screen.width()
 screen_height = screen.height()
 
@@ -66,8 +98,6 @@ else:
 col_spacing = screen_width * 0.03
 next_att_timer_label_size = int(screen_height * 0.05) * 1000
 
-#NOTE - is this necessary given GTK layout tools?
-#TODO - is a grid better
 light_box = Gtk.Box(spacing=col_spacing)
 vbox.pack_start(light_box, True, True, 0)
 
@@ -106,14 +136,12 @@ next_att_timer_box.pack_end(next_att_timer_label, True, True, 0)
 
 #set timer callback
 next_att_timer_start_dt = datetime.datetime.now()
-GObject.timeout_add(1000, next_att_timer_tick, next_att_timer_label, next_att_timer_label_size, next_att_timer_start_dt)
-
-#TODO - TEMP - demo drawing lift timer
-
-window.show_all()
+GObject.timeout_add(1000, timer_tick, next_att_timer_label, next_att_timer_label_size, next_att_timer_start_dt, 60, 'Next attempt submission:')
 
 #TEMP - exit on keypress
-window.connect('key-press-event', Gtk.main_quit)
+lift_timer_window.connect('key-press-event', handle_key_press)
+light_window.connect('key-press-event', handle_key_press)
+
 Gtk.main()
 
 
