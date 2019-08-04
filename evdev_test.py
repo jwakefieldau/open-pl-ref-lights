e2ba6ab8d2053c527ecceba122e541ced2efc33c
James Wakefield <James.Wakefield@cosive.com>
2019-08-04 17:59:32 +1000

    add intermittent read testing for evdev

--- evdev_test.py
+++ evdev_test.py
@@ -2,6 +2,8 @@
 # requires python3
 import evdev
 
+import time
+
 device_map = {path: evdev.InputDevice(path) for path in evdev.list_devices()}
 
 for (path, device,) in device_map.items():
@@ -15,11 +17,24 @@ for (path, device,) in device_map.items():
     if device.name.strip() == 'USB gamepad':
         gamepad_device = device
         print('-----------------------')
-        print('detected USB gamepad, listening for events forever')
+        print('detected USB gamepad, listening for events')
         print('-----------------------')
         break
 
-for event in gamepad_device.read_loop():
-    print(evdev.categorize(event))
+#for event in gamepad_device.read_loop():
+    #print(evdev.categorize(event))
         
+# try reading intermittently to imitate what will happen when we run in the Gtk main loop
+# do events get queued up or lost?
+while True:
+    print('sleeping for 5 seconds')
+    time.sleep(5.0)
+    print('awake!')
+ 
+    # this throws BlockingIOError if there are no events queued up to read
+    try:
+        for event in gamepad_device.read():
+            print(evdev.categorize(event))
 
+    except BlockingIOError:
+        pass
