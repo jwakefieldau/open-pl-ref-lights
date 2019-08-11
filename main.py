8391cfa4026a41d4022cb63f3a8aaf0611b38241
James <jmw1982@gmail.com>
2019-08-11 14:09:10 +1000

    progress commit refactor

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-import sys
+from configparser import ConfigParser
 
-class LightsApplication(Gtk.Application):
+from windows import LiftTimerWindow, LightsWindow
 
-    def __init__(self):
-        Gtk.Application.__init__(self)
-
-    def do_activate(self):
-        #TODO - windows
+config_path = './config.cfg'
 
 if __name__ == '__main__':
-    app = LightsApplication()
-    exit_status = app.run(sys.argv)
-    sys.exit(exit_status)
+
+    config = ConfigParser(config_path)
+
+    lift_timer_window = LiftTimerWindow(config['widget_scaling'])
+    lights_window = LightsWindow(config['widget_scaling'])
+
+    #show lift timer initially
+    lift_timer_window.show_all()
+    lift_timer_window.fullscreen()
+
+    Gtk.main()
+
--- windows.py
+++ windows.py
@@ -1,11 +1,51 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
-class LiftTimerWindow(Gtk.Window):
+class AbsAppWindow(Gtk.Window):
 
-    #TODO
+    #TODO - set up params to take from main app, config,
+    # related to widget scaling etc
+    def __init__(self, next_att_timer_scale):
 
-class LightsWindow(Gtk.Window):
+        if self.__class__.__name__ == 'AbsAppWindow':
+            raise NotImplementedError('Abstract base class') 
+    
+        screen = self.get_screen()
+        self.screen_width = screen.width
+        self.screen_height = screen.height
 
-    #TODO
+        self.next_att_timer_label_size = int(self.screen_height * next_att_timer_scale) * 1000
+
+        self.black_color = Gdk.Color(red=0, green=0, blue=0)
+        self.bg_eventbox = Gtk.EventBox()
+        self.add(bg_eventbox)
+        
+        Gtk.Window.__init__(self)
+
+    def update_next_att_timer(self, timer_str):
+        #TODO
+
+
+class LiftTimerWindow(AbsAppWindow):
+
+    def __init__(self, widget_scaling_dict):
+    
+        AbsAppWindow.__init__(self, widget_scaling_dict['next_att_timer_scale'])
+
+        self.lift_timer_label_size = self.screen_height * widget_scaling_dict['lift_timer_scale']
+        self.lift_timer_label = Gtk.Label()
+        self.bg_eventbox.add(self.lift_timer_label)
+
+    def update_lift_timer(self, timer_str):
+
+        self.lift_timer_label.set_markup('<span size="{}" foreground="white">{}</span>'.format(self.lift_timer_label_size, timer_str)) 
+        
+
+class LightsWindow(AbsAppWindow):
+
+    def __init__(self, widget_scaling_dict):
+    
+        AbsAppWindow.__init__(self, widget_scaling_dict['next_att_timer_scale'])
+
+        #TODO - method(s) for showing lights, next att timer
