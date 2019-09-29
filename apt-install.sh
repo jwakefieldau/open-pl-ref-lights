29aa55b3bd8f6ead7048378523aa27d68b48fcb6
James <jmw1982@gmail.com>
2019-09-29 17:55:28 +1000

    only do package installs as root, that way autostart file goes in proper dirZ

--- apt-install.sh
+++ apt-install.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # from http://www.peteronion.org.uk/PyGobjectGtk+3/PyGtk.html
 # internet says raspbian buster (which we should have on an rpi 4) has python 3 packaged
-apt-get install -y python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0 python3-evdev
+sudo apt-get install -y python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0 python3-evdev
 mkdir -p ~/.config/autostart
 cp open-pl-ref-lights.desktop ~/.config/autostart
